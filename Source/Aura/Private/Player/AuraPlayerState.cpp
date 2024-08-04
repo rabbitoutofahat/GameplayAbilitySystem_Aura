d0db0974b337d1317033b1ac7a9575702c05cd08
rabbitoutofahat <72710404+rabbitoutofahat@users.noreply.github.com>
2024-08-04 12:21:09 +0100

    The Player State

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,12 @@
+// Copyright Druid Mechanics
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+
+	NetUpdateFrequency = 100.f; // How often the server will try to update clients
+
+
+}
