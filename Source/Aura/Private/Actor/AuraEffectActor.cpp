c59eda65fc54df88af1f85c65d743c6a8b9e6834
rabbitoutofahat <72710404+rabbitoutofahat@users.noreply.github.com>
2024-08-07 16:49:51 +0100

    Instant Gameplay Effects

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -22,21 +22,21 @@ void AAuraEffectActor::BeginPlay()
 
 }
 
-void AAuraEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
 
-	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target); // A way to get an ASC of any actor in blueprint
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor); // A way to get an ASC of any actor in blueprint
 	if (TargetASC == nullptr) return;
 
 	check(GameplayEffectClass);
 	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext(); // Makes effect context and returns a FGameplayEffectContext handle
 	EffectContextHandle.AddSourceObject(this); // "What object caused the effect?"
-	FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
 	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get()); 
 	/** 
-		* Take EffectSpecHandle, get the data which is a TSharedPtr storing a FGameplayEffectSpec pointer in its raw form, call Get to get that raw pointer, 
-		* then deference everything into the FGameplayEffectSpec that the ApplyGameplayEffectSpecToSelf function wants.
-		*/
+	* Take EffectSpecHandle, get the data which is a TSharedPtr storing a FGameplayEffectSpec pointer in its raw form, call Get to get that raw pointer, 
+	* then deference everything into the FGameplayEffectSpec that the ApplyGameplayEffectSpecToSelf function wants.
+	*/
 
 }
 
